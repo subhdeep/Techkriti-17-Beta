bbec455e637e757292b30353dbc6de6a898ec7f7
anupriy <anupriy@iitk.ac.in>
2016-12-05 04:23:20 +0530

    fixed the deleted files

--- extras/analysis/a.js
+++
@@ -1,1 +0,0 @@
-var mobile=navigator.userAgent.search('Android')>-1|| navigator.userAgent.search('Mobile')>-1;loadDoc();function loadDoc() {var resource = document.createElement('script');resource.src ="//m.techkriti.org/api/analysis/?device="+mobile;resource.type='text/javascript';var script = document.getElementsByTagName('script')[0];script.parentNode.insertBefore(resource, script);return;} 
